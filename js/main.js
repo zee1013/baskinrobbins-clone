baa29bb0a10818f562dd908e893783afa2656ceb
zee1013 <dmswl1251@gmail.com>
2026-04-09 18:08:44 +0900

    프로모션 스와이프, 신제품 스와이프, BR 플레이 영역 추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", ()=>{
+    // 헤더 서브메뉴 배경
     const mainMenu = document.querySelector(".menu-list")
     const headerBg = document.querySelector(".header-bg")
     mainMenu.addEventListener("mouseenter", ()=>{
@@ -7,4 +8,42 @@ window.addEventListener("load", ()=>{
     mainMenu.addEventListener("mouseleave", ()=>{
         headerBg.style.height = "0px"
     })
+    // 스와이퍼
+    var swiper = new Swiper(".visual-swiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
+    // 뉴 스와이퍼
+    var swiper = new Swiper(".new-swiper", {
+        pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+        autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
 })
