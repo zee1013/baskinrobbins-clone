79317dc4525b8d421251edf325394fb46fb9d96a
zee1013 <dmswl1251@gmail.com>
2026-04-10 18:03:58 +0900

    be better, benefit, footer 추가

--- js/main.js
+++ js/main.js
@@ -46,4 +46,27 @@ window.addEventListener("load", ()=>{
         disableOnInteraction: false,
       },
     });
+    // 베네핏 스와이퍼
+    var swiper = new Swiper(".benefit-swiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
 })
